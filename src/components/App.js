1a7c5d417561ab782509f6d06768727a8d9d448b
VictoriaCarvalho <vix219@icloud.com>
2025-02-17 11:35:48 -0600

    add lab

--- src/components/App.js
+++ src/components/App.js
@@ -13,18 +13,31 @@ function Home() {
     <div id="home">
       <h1>Home</h1>
     </div>
+
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <div>
+        <Home />
+      {Home} 
+      </div>
+      <div>
+        <About />
+        {About}
+      </div>
     </div>
   );
 }
